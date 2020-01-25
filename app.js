b3183e211bec54f48c3aac95fb108df154877c17
Max Lewis <max.a.lewis98@gmail.com>
2020-01-25 10:15:56 -0700

    its single player again but functional

--- app.js
+++ app.js
@@ -76,14 +76,15 @@ function breakTime() {
 
 // when data changes this will run
 database.ref().on("value", function(snapshot) {
-
+    console.log(snapshot.val())
     // if the game logic value is true run game logic then reset runlogic value
-    if (snapshot.child("RunLogic").val()&&player1Select){
+    if (snapshot.child("RunLogic").val()){
 
         Player1Choice = snapshot.child("Player1Choice").val()
-        Player2Choice = snapshot.child("Player2Choice").val()
+        // Player2Choice = snapshot.child("Player2Choice").val()
 
-        // Player2Choice = tempArray[Math.floor(Math.random() * tempArray.length)]
+        Player2Choice = tempArray[Math.floor(Math.random() * tempArray.length)]
+
 
         // 1 double default
         // 1 tie
@@ -92,117 +93,65 @@ database.ref().on("value", function(snapshot) {
         // 3 player 2 victories
         if(Player1Choice==="a"&&Player2Choice==="a"){
             console.log("both defaulted")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Ties: snapshot.child("Ties").val()+1
-            })
             $("#ties").text(snapshot.child("Ties").val())
             $("#jumbo-3").text("Both Players defaulted")
         } else if(Player1Choice===Player2Choice){
             console.log("tie")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Ties: snapshot.child("Ties").val()+1
-            })
             $("#ties").text(snapshot.child("Ties").val())
             $("#jumbo-3").text("Tie")
         } else if(Player1Choice==="a"){
             console.log("Player1 defaulted")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Player2Wins: snapshot.child("Player2Wins").val()+1
-            })
             $("#player2wins").text(snapshot.child("Player2Wins").val())
             $("#jumbo-3").text("Player 1 defaulted")
         } else if(Player2Choice==="a"){
             console.log("Player2 defaulted")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Player1Wins: snapshot.child("Player1Wins").val()+1
-            })
             $("#player1wins").text(snapshot.child("Player1Wins").val())
             $("#jumbo-3").text("Player 2 defaulted")
         } else if(Player1Choice==="r"&&Player2Choice==="s"){
             console.log("Player1 Won")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Player1Wins: snapshot.child("Player1Wins").val()+1
-            })
             $("#player1wins").text(snapshot.child("Player1Wins").val())
             $("#jumbo-3").text("Player 1 won with rock")
         } else if(Player1Choice==="s"&&Player2Choice==="p"){
             console.log("Player1 Won")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Player1Wins: snapshot.child("Player1Wins").val()+1
-            })
             $("#player1wins").text(snapshot.child("Player1Wins").val())
             $("#jumbo-3").text("Player 1 won with scissors")
         } else if(Player1Choice==="p"&&Player2Choice==="r"){
             console.log("Player1 Won")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Player1Wins: snapshot.child("Player1Wins").val()+1
-            })
             $("#player1wins").text(snapshot.child("Player1Wins").val())
             $("#jumbo-3").text("Player 1 wins with paper")
         } else if(Player2Choice==="r"&&Player1Choice==="s"){
             console.log("Player2 Won")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Player2Wins: snapshot.child("Player2Wins").val()+1
-            })
             $("#player2wins").text(snapshot.child("Player2Wins").val())
             $("#jumbo-3").text("Player 2 wins with rock")
         } else if(Player2Choice==="s"&&Player1Choice==="p"){
             console.log("Player2 Won")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Player2Wins: snapshot.child("Player2Wins").val()+1
-            })
             $("#player2wins").text(snapshot.child("Player2Wins").val())
             $("#jumbo-3").text("Player 2 wins with scissors")
         } else if(Player2Choice==="p"&&Player1Choice==="r"){
             console.log("Player2 Won")
-            database.ref().update({
-                RunLogic: false,
-                Player1Choice: "a",
-                Player2Choice: "a",
-                Player2Wins: snapshot.child("Player2Wins").val()+1
-            })
             $("#player2wins").text(snapshot.child("Player2Wins").val())
             $("#jumbo-3").text("Player 2 wins with paper")
         } else {
             console.log("somethings wrong")
         }
 
+        // without runlogic data updates wont run the gamelogic
+        // reset to a so players can default for AFK
+        database.ref().update({
+            RunLogic: false,
+            Player1Choice: "a",
+            Player2Choice: "a",
+        })
     }
 
+    // game start variable allows for this to only run once once both players check in
     // if snapshot.child("player1") && player 2 start game cycle
     if(!snapshot.child("GameStart").val()){
         if (snapshot.child("Player1").val() && snapshot.child("Player2").val()){
+            runBreak()
             database.ref().update({
                 GameStart: true,
             })
-            runBreak()
         }
     }
 })
@@ -212,28 +161,28 @@ $(document).ready(function() {
 
     // player1/2 select button
     $("#select-player1").on("click",function(){
-        if(playerSelected){
-            return false
-        }
+        // if(playerSelected){
+        //     return false
+        // }
         $("#jumbo-2").text("You are player1")
         $("#current-player").text("1")
         database.ref().update({
             Player1: true,
         })
-        playerSelected = true
-        player1Select = true
+        // playerSelected = true
+        // player1Select = true
     })
     $("#select-player2").on("click",function(){
-        if(playerSelected){
-            return false
-        }
+        // if(playerSelected){
+        //     return false
+        // }
         $("#jumbo-3").text("You are player2")
         $("#current-player").text("2")
         database.ref().update({
             Player2: true,
         })
-        playerSelected = true
-        player2Select = true
+        // playerSelected = true
+        // player2Select = true
     })
 
     // rockpaper scissors buttons
@@ -244,15 +193,19 @@ $(document).ready(function() {
         }
         $("#jumbo-3").text("You chose ("+$(this).val()+")")
 
-        if(player1Select){
-            database.ref().update({
-                Player1Choice: $(this).val(),
-            })
-        } else {
-            database.ref().update({
-                Player2Choice: $(this).val(),
-            })
-        }
+        database.ref().update({
+            Player1Choice: $(this).val(),
+        })
+
+        // if(player1Select){
+        //     database.ref().update({
+        //         Player1Choice: $(this).val(),
+        //     })
+        // } else {
+        //     database.ref().update({
+        //         Player2Choice: $(this).val(),
+        //     })
+        // }
     })
     
     // reset button
